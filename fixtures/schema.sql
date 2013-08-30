78dccc5b692761d7ce6ea6df990ebbcd528fd82b
Tomaz Muraus <tomaz@tomaz.me>
2013-08-30 15:48:28 +0200

    Update schema ddl.

--- fixtures/schema.sql
+++ fixtures/schema.sql
@@ -1,11 +1,12 @@
 BEGIN;
 
-CREATE TABLE IF NOT EXISTS refesh_tokens (
+CREATE TABLE IF NOT EXISTS refresh_tokens (
   user_id TEXT PRIMARY KEY,
   refresh_token TEXT NOT NULL,
 
-  UNIQUE(user_id, refresh_token)
+  UNIQUE(user_id),
+  UNIQUE(refresh_token)
 );
 
-CREATE INDEX refresh_token_idx ON refesh_tokens (refresh_token);
+CREATE INDEX refresh_token_idx ON refresh_tokens (refresh_token);
 COMMIT;
