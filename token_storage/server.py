c615fbc8acf50588621684d8d6ac42d58a116b41
Tomaz Muraus <tomaz@tomaz.me>
2013-08-30 16:03:29 +0200

    Hook up command line options.

--- token_storage/server.py
+++ token_storage/server.py
@@ -1,18 +1,29 @@
 import ssl
 
+from tornado.options import define, options
+from tornado.options import parse_command_line
 from tornado.web import Application
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
 from token_storage.urls import urls
 
+define('cert_file', default='fixtures/server/certs/server.crt',
+       help='Path to the server certificate file')
+define('key_file', default='fixtures/server/keys/server.key',
+       help='Path to the server certificate key file')
+define('ca_file', default='fixtures/ca/myCA.crt',
+       help='Path to the CA file')
+
 
 def run_server():
-    application = Application(urls, debug=True)
+    parse_command_line()
+    debug = options.logging == 'debug'
+    application = Application(urls, debug=debug)
     ssl_options = dict(
-        certfile='fixtures/server/certs/server.crt',
-        keyfile='fixtures/server/keys/server.key',
-        ca_certs='fixtures/ca/myCA.crt',
+        certfile=options.cert_file,
+        keyfile=options.key_file,
+        ca_certs=options.ca_file,
         cert_reqs=ssl.CERT_REQUIRED,
     )
 
