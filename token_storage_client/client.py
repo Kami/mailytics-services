e3c6368f2f3c2fd6d14cd192fd95a6118d49e7e9
Tomaz Muraus <tomaz@tomaz.me>
2013-09-02 16:48:14 +0200

    Add basic client implementation.

---
+++ token_storage_client/client.py
@@ -0,0 +1,51 @@
+import random
+import httplib
+import json
+
+import requests
+from requests.exceptions import ConnectionError
+
+from token_storage_client.utils import method_decorator, retry_on_error
+
+
+class BaseClient(object):
+    def __init__(self, server_urls, client_cert, client_key, ca_certs):
+        self._server_urls = server_urls
+        self._client_cert = client_cert
+        self._client_key = client_key
+        self._ca_certs = ca_certs
+
+        self._verify_arg = self._ca_certs
+        self._cert_arg = (self._client_cert, self._client_key)
+
+
+class GetClient(BaseClient):
+    @method_decorator(retry_on_error(retry_count=3,
+                                     exceptions=(ConnectionError,)))
+    def get(self, user_id):
+        url = random.choice(self._server_urls)
+        values = {'url': url, 'user_id': user_id}
+        url = '%(url)s//users/%(user_id)s/refresh_token' % values
+        response = requests.get(url, cert=self._cert_arg,
+                                verify=self._verify_arg)
+        data = response.json()
+        return data['refresh_token']
+
+
+class SetClient(BaseClient):
+    @method_decorator(retry_on_error(retry_count=3,
+                      exceptions=(ConnectionError,)))
+    def set(self, user_id, refresh_token):
+        # TODO: This method should try to re-try
+        url = random.choice(self._server_urls)
+        values = {'url': url, 'user_id': user_id}
+        url = '%(url)s//users/%(user_id)s/refresh_token' % values
+        data = json.dumps({'refresh_token': refresh_token})
+
+        response = requests.put(url, data=data, cert=self._cert_arg,
+                                verify=self._verify_arg)
+        if response.status_code != httplib.NO_CONTENT:
+            raise Exception('Failed to save refresh token: %s' %
+                            (response.body))
+
+        return True
---
+++ token_storage_client/utils.py
@@ -0,0 +1,69 @@
+import time
+import logging
+from functools import wraps, update_wrapper
+
+logger = logging.getLogger('token_storage_client.utils')
+
+
+def retry_on_error(retry_count, delay=1, backoff=1.5, exceptions=(Exception,)):
+    if not isinstance(exceptions, (tuple, list)):
+        exceptions = tuple([exceptions])
+
+    def catch_exception(func):
+        def decorator(*args, **kwargs):
+            m_retry_count = retry_count
+            m_delay = delay
+
+            while m_retry_count > 0:
+                try:
+                    return_value = func(*args, **kwargs)
+                except exceptions, e:
+                    logger.debug('Method %(name)s throw an exception: '
+                                 '"%(e)s", retrying...' %
+                                 {'name': func.__name__, 'e': str(e)})
+                    m_retry_count -= 1
+                    time.sleep(m_delay)
+
+                    m_delay *= backoff
+                else:
+                    return return_value
+
+        return wraps(func)(decorator)
+    return catch_exception
+
+
+# Taken from Django
+# https://github.com/django/django/blob/master/django/utils/decorators.py
+def method_decorator(decorator):
+    """
+    Converts a function decorator into a method decorator
+    """
+    # 'func' is a function at the time it is passed to _dec, but will
+    # eventually
+    # be a method of the class it is defined it.
+    def _dec(func):
+        def _wrapper(self, *args, **kwargs):
+            @decorator
+            def bound_func(*args2, **kwargs2):
+                return func(self, *args2, **kwargs2)
+            # bound_func has the signature that 'decorator' expects i.e.  no
+            # 'self' argument, but it is a closure over self so it can call
+            # 'func' correctly.
+            return bound_func(*args, **kwargs)
+        # In case 'decorator' adds attributes to the function it decorates, we
+        # want to copy those. We don't have access to bound_func in this scope,
+        # but we can cheat by using it on a dummy function.
+
+        @decorator
+        def dummy(*args, **kwargs):
+            pass
+        update_wrapper(_wrapper, dummy)
+        # Need to preserve any existing attributes of 'func', including the
+        # name.
+        update_wrapper(_wrapper, func)
+
+        return _wrapper
+    update_wrapper(_dec, decorator)
+    # Change the name to aid debugging.
+    _dec.__name__ = 'method_decorator(%s)' % decorator.__name__
+    return _dec
