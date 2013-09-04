ad3ae491be120f4b6d34e4067b5677dfe954cfa8
Tomaz Muraus <tomaz@tomaz.me>
2013-09-04 23:46:46 +0200

    Allow user to specify secrets_path option.

--- token_storage_server/utils.py
+++ token_storage_server/utils.py
@@ -3,13 +3,25 @@ import time
 import httplib
 from urllib import urlencode
 
+from tornado.options import options
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 from tornado.gen import Task, engine
 
 TOKEN_URI = 'https://accounts.google.com/o/oauth2/token'
+CLIENT_SECRETS = None
 
-data = open('./client_secrets.json').read()
-CLIENT_SECRETS = json.loads(data)['web']
+
+def get_secret(key):
+    global CLIENT_SECRETS
+
+    if not CLIENT_SECRETS:
+        with open(options.secrets_path) as fp:
+            data = fp.read()
+
+        CLIENT_SECRETS = json.loads(data)['web']
+
+    return CLIENT_SECRETS[key]
+
 
 @engine
 def get_new_access_token(refresh_token, callback):
@@ -19,8 +31,8 @@ def get_new_access_token(refresh_token, callback):
 
     Return a ``dict`` with access_token and expires_at key.
     """
-    client_id = CLIENT_SECRETS['client_id']
-    client_secret = CLIENT_SECRETS['client_secret']
+    client_id = get_secret('client_id')
+    client_secret = get_secret('client_secret')
 
     payload = {'refresh_token': refresh_token, 'client_id': client_id,
                'client_secret': client_secret, 'grant_type': 'refresh_token'}
