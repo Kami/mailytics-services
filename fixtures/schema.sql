2ab48385f4b2dd4e8c05b1aa28c12db9e87db96b
Tomaz Muraus <tomaz@tomaz.me>
2013-09-04 22:43:38 +0200

    Add new access_tokens_cache table.

--- fixtures/schema.sql
+++ fixtures/schema.sql
@@ -10,3 +10,14 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
 
 CREATE INDEX refresh_token_idx ON refresh_tokens (refresh_token);
 COMMIT;
+
+BEGIN;
+CREATE TABLE IF NOT EXISTS access_tokens_cache (
+  user_id TEXT PRIMARY KEY,
+  access_token TEXT NOT NULL,
+  expires_at INTEGER NOT NULL,
+
+  UNIQUE(user_id)
+);
+
+COMMIT;
